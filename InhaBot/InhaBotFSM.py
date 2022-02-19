a84601655ec256ad95b1e501263506f34c05c3c4
Byun Jaewoo <qus7146@naver.com>
2022-02-19 18:11:57 +0900

    2022.02.19

--- InhaBot/InhaBotFSM.py
+++ InhaBot/InhaBotFSM.py
@@ -1,51 +1,49 @@
-from flask import Flask, json, request, jsonify
-import sys
-from flask import Flask, json, request, jsonify
-import sys
+from flask import Flask, json, request, jsonify, Blueprint
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import json
 from datetime import datetime
-import threading
-import time
-import requests
-from dateutil.relativedelta import relativedelta
+
 
 app = Flask(__name__)
 
 global datas
 global texts
+
+global Today
 Today = datetime.today().strftime('%Y-%m-%d')
-
-def WeekDateGet():
-    format = '%Y-%m-%d'
-
-    WeekTexts = ""
-    for WeekDate in datas:
-            if Today == WeekDate:
-                NewWeekDate = WeekDate
-                for days in range(1, 8):
-                    print(datas[NewWeekDate])
-                    #SendMessage(datas[A],Today)
-                    WeekTexts += "D-" + str(days-1) + "\n"
-                    WeekTexts += "- " + str(datas[NewWeekDate]).replace("[","").replace("]","").replace(",","\n-").replace("\'","") + "\n\n"
-                    NewWeekDate = str(datetime.strptime(WeekDate, format) + relativedelta(days = days)).split(" ")[0]
-    WeekTexts[-1] = ""
-    WeekTexts[-1] = ""
-    return WeekTexts
-
 file_path = "./UniversityClassList.json"
 
 with open(file_path, 'r', encoding="utf-8") as file:
     datas = json.load(file)
 
+def WeekDateGet():
+    format = '%Y-%m-%d'
+    WeekTexts = ""
+    Today = datetime.today().strftime('%Y-%m-%d')
+    for WeekDate in datas:
+        if Today == WeekDate:
+            NewWeekDate = WeekDate
+            for days in range(1, 8):
+                print(datas[NewWeekDate])
+                #SendMessage(datas[A],Today)
+                WeekTexts += "D-" + str(days-1) + "(" + str(int(Today.split("-")[2])+days-1) + "일)" + "\n"
+                WeekTexts += "- " + str(datas[NewWeekDate]).replace("[","").replace("]","").replace(",","\n-").replace("\'","") + "\n\n"
+                NewWeekDate = str(datetime.strptime(WeekDate, format) + relativedelta(days = days)).split(" ")[0]
+    WeekTexts = WeekTexts[:-2]
+    return WeekTexts
+
 @app.route('/message', methods=['POST'])
 def Message():
-    #print("실행중입니다")
+    Today = datetime.today().strftime('%Y-%m-%d')
+    print("실행중입니다")
     texts = ""
     content = request.get_json()
-    print(content['userRequest']['user']['id'])
+    print(content)
+    #print(content['userRequest']['user']['id'])
     content = content['action']['name']
     #content=content.replace("\n","")
-    print(content)
+    #print(content)
     for A in datas:
         if Today == A:
             #SendMessage(datas[A],Today)
@@ -53,7 +51,7 @@ def Message():
     if texts == "":
         texts = "존재하지 않습니다"
     #print(texts)
-    if content == u"테스트":
+    if content == u"오늘의일정":
         dataSend = {
             "version" : "2.0",
             "template" : {
@@ -66,14 +64,14 @@ def Message():
                 ]
             }
         }
-    elif content == u"주간일정확인":
+    elif content == u"주간학사일정":
         dataSend = {
             "version" : "2.0",
             "template" : {
                 "outputs" : [
                     {
                         "simpleText" : {
-                            "text" : "이번주 일정 : {}".format(WeekDateGet())   #<---------- WeekDateGet function Error
+                            "text" : "이번주 일정 : \n {}".format(WeekDateGet())   #<---------- WeekDateGet function Error
                         }
                     }
                 ]
@@ -94,5 +92,6 @@ def Message():
         }
     return jsonify(dataSend)
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=5000, debug=True)
